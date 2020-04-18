23abca0b1f140be7582bcfdb3c18ff22e5f210f1
Wei Chen <weichen@apache.org>
2020-04-18 12:42:50 +0800

    push-init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Following Me or Not
+
+There are people on Github who follows a lot of people and will unfollow you if you follow them back.
+
+This is a simple page for me to track that.
+
+For those who will want to use this without forking the repo,
+you can modify the uid in the page params:
+
+```
+?uid=Wei-1
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+const params = new URLSearchParams(document.location.search);
+const uid = (params.get("uid") === undefined || params.get("uid") == null) ?
+  "Wei-1" : params.get("uid");
+console.log(uid);
+
+var data = {"followers":[],"following":[]}
+var req = new XMLHttpRequest();
+var latestType = "followers"
+req.onload = function() {
+  if (this.status >= 200 && this.status < 400) {
+    // Success!
+    var tmp = JSON.parse(this.response);
+    console.log(tmp);
+    data[latestType] = tmp;
+    ReactDOM.render(
+      <Report />,
+      document.getElementById('content')
+    );
+  }
+};
+function getOutput(reqtype) {
+  latestType = reqtype;
+  req.open('GET', 'https://api.github.com/users/' +
+    uid + '/' + reqtype + '?per_page=100', true);
+  req.send();
+}
+getOutput('followers');
+setTimeout(function() {getOutput('following');}, 1000);
+
+function getColor(check) {
+  if (check) return {color:'#0f0'};
+  else return {color:'#f00'};
+}
+function checkFollow(id) {
+  var check = false
+  data['followers'].forEach(user => {
+    if (user.id == id) check = true;
+  });
+  return <h4 style={getColor(check)}> {check + ""} </h4>;
+}
+function Report() {
+  var stats = data['following'].map(function(user, index) {
+    return (
+      <tr>
+        <td><img src={user.avatar_url} width="20" /></td>
+        <td><a href={user.html_url}><h4> {user.login} </h4></a></td>
+        <td> {checkFollow(user.id)} </td>
+      </tr>
+    );
+  }, this);
+  return (
+    <table>
+      <tr><th/><th>User</th><th>Following Me or Not</th></tr>
+      {stats}
+    </table>
+  );
+};
