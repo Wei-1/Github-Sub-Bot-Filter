f2d0c6ebcace5777edcacc4a18930744a5cc81f3
Wei Chen <weichen@apache.org>
2024-06-25 09:12:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let renderMapping = {
   4: "Organization"
 };
 let orgs = ["OpenLive3D"];
-let bots = ["vjanz", "imaarov", "Rafaelmdcarneiro", "yuuire"];
+let bots = ["vjanz", "imaarov", "Rafaelmdcarneiro", "yuuire", "libraiger", "itsparsh10", "sreya-satheesh"];
 let allUserChecks = {};
 let allUserObjs = {};
 function dataOrganization() {
