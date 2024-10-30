3b72a01dd19bac6a2f69116a70bf74117fb4a98b
Wei Chen <weichen@apache.org>
2024-10-30 12:15:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let bots = ["vjanz", "imaarov", "Rafaelmdcarneiro", "yuuire", "libraiger", "itsp
             "PremChapagain", "ekrishnachaitanya2004", "renominated", "rainbowdev1359",
             "YoussefMoHlemyAlpha", "Keviiiiiiiin", "kentaurse", "topsecretagent007", "xopaz",
             "fairsky0201", "CelaDaniel", "BusraEcemSakar", "formidablae", "Tohidkhan6332",
-            "meliksahyorulmazlar", "AZIZEMESR", "mooosakhan"];
+            "meliksahyorulmazlar", "AZIZEMESR", "mooosakhan", "fIuffy"];
 let allUserChecks = {};
 let allUserObjs = {};
 function dataOrganization() {
